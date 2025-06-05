f29a1fb52ee9567ef30c91a24e7f6a155f6317f0
kellerkatlin <kellerkatlin.k@gmail.com>
2025-06-05 02:13:47 -0500

    feat: Update Compra dialog to use select inputs for Producto and Proveedor with dynamic loading

--- src/app/components/cevicheria/compra-dialog/compra-dialog.component.ts
+++ src/app/components/cevicheria/compra-dialog/compra-dialog.component.ts
@@ -1,16 +1,39 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {Compra} from '@services/compra.service';
+import {Producto, ProductosService} from '@services/productos.service';
+import {Proveedor, ProveedoresService} from '@services/proveedores.service';
 
 @Component({
     selector: 'app-compra-dialog',
     templateUrl: './compra-dialog.component.html',
     styleUrl: './compra-dialog.component.scss'
 })
-export class CompraDialogComponent {
+export class CompraDialogComponent implements OnInit {
     readonly dialogRef = inject(MatDialogRef<CompraDialogComponent>);
     readonly data = inject<Compra>(MAT_DIALOG_DATA);
+    productosOptions: Producto[] = [];
+    proveedoresOptions: Proveedor[] = [];
+    private readonly productoService = inject(ProductosService);
+    private readonly proveedorService = inject(ProveedoresService);
 
+    ngOnInit(): void {
+        this.productoService.getAll().subscribe((productos) => {
+            this.productosOptions = productos;
+        });
+
+        this.proveedorService.getAll().subscribe((proveedores) => {
+            this.proveedoresOptions = proveedores;
+        });
+    }
+
+    onProductoChange(id: number) {
+        this.data.producto = {id};
+    }
+
+    onProveedorChange(id: number) {
+        this.data.proveedor = {id};
+    }
     save() {
         this.dialogRef.close(this.data);
     }
